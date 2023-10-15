1aa3ff1336793ec9b5bd229af5c2cd0c7f00b2ce
RealNqm <147895086+RealNqm@users.noreply.github.com>
2023-10-15 11:10:39 +0700

    Add files via upload

--- src/group1/util/lists/BinarySearchNode.java
+++ src/group1/util/lists/BinarySearchNode.java
@@ -14,9 +14,11 @@ public class BinarySearchNode<T extends Comparable<T>>
     }
     
     public BinarySearchNode() {
+        value = null;
         left = right = null;
     }
     
+    //1.2 insert data
     public void insert(T data){
         if(value == null){
             value = data;
@@ -39,6 +41,7 @@ public class BinarySearchNode<T extends Comparable<T>>
         }
     }
     
+    //1.3 inorder tarverse
     public ArrayList<T> inorder(){
         ArrayList<T> element = new ArrayList<>();
         if(left != null){
@@ -51,6 +54,7 @@ public class BinarySearchNode<T extends Comparable<T>>
         return element;
     }
     
+    //calulate height (not necessary)
     private int height(BinarySearchNode n){
         if(n == null){
             return 0;
@@ -65,8 +69,113 @@ public class BinarySearchNode<T extends Comparable<T>>
         return height(this);
     }
     
-    public ArrayList<T> breadth(){
-        ArrayList<T> element = new ArrayList<>();
+    //1.4 breadth first traverse
+    public ArrayList<T> levelOrderTraversal() {
+        ArrayList<T> result = new ArrayList<>();
+        ArrayList<BinarySearchNode> currentLevel = new ArrayList<>();
+        ArrayList<BinarySearchNode> nextLevel = new ArrayList<>();
+
+        currentLevel.add(this);
+
+        while (!currentLevel.isEmpty()) {
+            for (BinarySearchNode node : currentLevel) {
+                result.add((T)node.value);
+                if (node.left != null) {
+                    nextLevel.add(node.left);
+                }
+                if (node.right != null) {
+                    nextLevel.add(node.right);
+                }
+            }
+
+            currentLevel = new ArrayList<>(nextLevel);
+            nextLevel.clear();
+        }
+
+        return result;
+    }
+    // count number of node
+    private int count(BinarySearchNode n){
+        if(n == null){
+            return 0;
+        }
+        int leftcount = count(n.left);
+        int rightcount = count(n.right);
         
+        return leftcount + rightcount + 1;
+    }
+    
+    public int count(){
+        return count(this);
+    }
+    
+    //1.8 simply balance
+    public BinarySearchNode<T> balance(ArrayList<T> sortedlist, int start, int end){
+        if(start > end){
+            return null;
+        }
+        int mid = (start + end)/2;
+        BinarySearchNode<T> n = new BinarySearchNode<>(sortedlist.get(mid));
+        
+        n.left = balance(sortedlist, start, mid - 1);
+        n.right = balance(sortedlist, mid + 1, end);
+        
+        return n;
+    }
+    
+    
+    public void delete(T data){
+        BinarySearchNode<T> parent = null;
+        BinarySearchNode<T> current = this;
+
+        while (current != null && current.value != data) {
+            parent = current;
+
+            if (data.compareTo(current.value) < 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        
+        if (current == null){
+            return;
+        }
+        
+        if(current.right == null && current.left == null){
+            if(parent != null){
+                if(parent.left == current){
+                    parent.left = null;
+                }
+                else{
+                    parent.right = null;
+                }
+            }
+            else{
+                current = null;
+            }
+        }
+        else if(current.right == null || current.left == null){
+            if(current.left != null){
+                current.value = current.left.value;
+                current.left = null;
+            }
+            else{
+                current.value = current.right.value;
+                current.right = null;
+            }
+        }
+        else{
+            BinarySearchNode<T> successor = findInOrderSuccessor(current.right);
+            current.value = successor.value;
+            current.right = null;
+        }
+    }
+    
+    private BinarySearchNode<T> findInOrderSuccessor(BinarySearchNode<T> node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
     }
 }
